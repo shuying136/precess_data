c6f86cfd81a9955b202486fe6726e0dd0d168fb9
shuying136 <shuying1369@163.com>
2017-03-25 12:22:09 +0800

    Create build_dataset.py

---
+++ build_dataset.py
@@ -0,0 +1,19 @@
+def build_dataset(words):
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count = unk_count + 1
+        data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reverse_dictionary
+    #处理文档的数据，建立one-hot数组
